30a95b4b5c2ae85a6697806d6e8759f9529c90b8
Fayne Guo <fayne.guo@gmail.com>
2015-09-19 20:40:00 +0800

    add routers

--- app/routes.php
+++ app/routes.php
@@ -11,18 +11,16 @@
 |
 */
 
-//Route::get('/', function () {
-//    return View::make('hello');
-//});
-
 /*
 * Home
 */
-Route::get('/', ['as' => 'dashboard.home', 'uses' => 'HomeController@welcome']);
+Route::get('/', ['as' => 'dashboard.home', 'before' => 'sentry.auth', 'uses' => 'HomeController@index']);
 
 /*
  * Auth
  */
 Route::get('login', ['as' => 'dashboard.login', 'uses' => 'AuthController@getLogin']);
+Route::get('register', ['as' => 'dashboard.register', 'uses' => 'AuthController@getRegister']);
 Route::post('login', ['as' => 'dashboard.login', 'uses' => 'AuthController@postLogin']);
+Route::post('register', ['as' => 'dashboard.register', 'uses' => 'AuthController@postRegister']);
 Route::get('logout', ['as' => 'dashboard.logout', 'uses' => 'AuthController@getLogout']);
