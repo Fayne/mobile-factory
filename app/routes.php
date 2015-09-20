1d218e79a7c95edd8db54027489ba6f6badd6ffc
Fayne Guo <fayne.guo@gmail.com>
2015-09-20 17:58:50 +0800

    update router

--- app/routes.php
+++ app/routes.php
@@ -14,7 +14,7 @@
 /*
 * Home
 */
-Route::get('/', ['as' => 'dashboard.home', 'before' => 'sentry.auth', 'uses' => 'HomeController@index']);
+Route::get('/', ['as' => 'dashboard.home', 'before' => 'sentry.auth|dashboard.nickname', 'uses' => 'HomeController@index']);
 
 /*
  * Auth
@@ -24,3 +24,27 @@ Route::get('register', ['as' => 'dashboard.register', 'uses' => 'AuthController@
 Route::post('login', ['as' => 'dashboard.login', 'uses' => 'AuthController@postLogin']);
 Route::post('register', ['as' => 'dashboard.register', 'uses' => 'AuthController@postRegister']);
 Route::get('logout', ['as' => 'dashboard.logout', 'uses' => 'AuthController@getLogout']);
+
+/*
+ * Dashboard
+ */
+Route::group(['prefix' => 'dashboard', 'before' => 'sentry.auth'], function()
+{
+
+    /*
+     * Nickname
+     */
+    Route::get('nickname', ['as' => 'dashboard.nickname', 'uses' => 'DashboardController@getNickname']);
+    Route::post('nickname', ['as' => 'dashboard.nickname', 'uses' => 'DashboardController@storeNickname']);
+
+});
+
+/*
+ * Orders
+ */
+Route::group(['prefix' => 'orders', 'before' => 'sentry.auth|dashboard.nickname'], function()
+{
+    Route::get('create', ['as' => 'orders.create', 'uses' => 'OrdersController@create']);
+    Route::get('create/success', ['as' => 'orders.created.success', 'uses' => 'OrdersController@createdSuccess']);
+
+});
