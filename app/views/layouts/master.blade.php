0f9a74133e57116fcb16f1b16d84c8e1e3880068
Fayne Guo <fayne.guo@gmail.com>
2015-09-30 14:29:33 +0800

    remove js files

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -39,25 +39,8 @@
     <script type="text/javascript" src="/src/plugins/nanoScroller/js/jquery.nanoscroller.min.js"></script>
     <!-- nano scroller -->
     <script type="text/javascript" src="/src/js/application.js"></script>
-    <script type="text/javascript" src="/src/demo/demo.js"></script>
-    <script type="text/javascript" src="/src/demo/demo-switcher.js"></script>
     <!-- End loading site level scripts -->
     <!-- Load page level scripts-->
-    <!-- Charts -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.min.js"></script>
-    <!-- Flot Main File -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.pie.min.js"></script>
-    <!-- Flot Pie Chart Plugin -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.stack.min.js"></script>
-    <!-- Flot Stacked Charts Plugin -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.orderBars.min.js"></script>
-    <!-- Flot Ordered Bars Plugin-->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.resize.min.js"></script>
-    <!-- Flot Responsive -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.tooltip.min.js"></script>
-    <!-- Flot Tooltips -->
-    <script type="text/javascript" src="/src/plugins/charts-flot/jquery.flot.spline.js"></script>
-    <!-- Flot Curved Lines -->
 
     <script type="text/javascript" src="/src/plugins/sparklines/jquery.sparklines.min.js"></script>
     <!-- Sparkline -->
@@ -67,7 +50,6 @@
     <!-- jVectorMap -->
     <script type="text/javascript" src="/src/plugins/switchery/switchery.js"></script>
     <!-- Switchery -->
-    <script type="text/javascript" src="/src/plugins/easypiechart/jquery.easypiechart.js"></script>
     <script type="text/javascript" src="/src/plugins/fullcalendar/moment.min.js"></script>
     <!-- Moment.js Dependency -->
     <script type="text/javascript" src="/src/plugins/fullcalendar/fullcalendar.min.js"></script>
@@ -89,20 +71,10 @@
     <script type="text/javascript" src="/dist/plugins/iCheck/icheck.min.js"></script>
     <script type="text/javascript" src="/dist/plugins/nanoScroller/js/jquery.nanoscroller.min.js"></script>
     <script type="text/javascript" src="/dist/js/application.js"></script>
-    <script type="text/javascript" src="/dist/demo/demo.js"></script>
-    <script type="text/javascript" src="/dist/demo/demo-switcher.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.pie.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.stack.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.orderBars.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.resize.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.tooltip.min.js"></script>
-    <script type="text/javascript" src="/dist/plugins/charts-flot/jquery.flot.spline.js"></script>
     <script type="text/javascript" src="/dist/plugins/sparklines/jquery.sparklines.min.js"></script>
     <script type="text/javascript" src="/dist/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
     <script type="text/javascript" src="/dist/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
     <script type="text/javascript" src="/dist/plugins/switchery/switchery.js"></script>
-    <script type="text/javascript" src="/dist/plugins/easypiechart/jquery.easypiechart.js"></script>
     <script type="text/javascript" src="/dist/plugins/fullcalendar/moment.min.js"></script>
     <script type="text/javascript" src="/dist/plugins/fullcalendar/fullcalendar.min.js"></script>
     {{--<script type="text/javascript" src="/dist/demo/demo-index.js"></script>--}}
