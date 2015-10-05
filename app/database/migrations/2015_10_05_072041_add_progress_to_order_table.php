34f8a9f62c678cf75b31ec7a4008d98224d9d46b
Fayne Guo <fayne.guo@gmail.com>
2015-10-05 21:45:04 +0800

    add progress field to orders table

---
+++ app/database/migrations/2015_10_05_072041_add_progress_to_order_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProgressToOrderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+            $table->integer('progress')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+            $table->dropColumn([
+                'progress',
+            ]);
+		});
+	}
+
+}
